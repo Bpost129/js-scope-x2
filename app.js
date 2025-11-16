154e74da253499f6e283c696d95c9dabcd88c68c
Bpost129 <Bpost129@yahoo.com>
2025-11-16 17:09:59 -0500

    demonstrate the difference between let and var

--- app.js
+++ app.js
@@ -33,3 +33,35 @@ function foo(x) {
 }
 
 console.log(foo(a))
+
+// you can look out, but you can't look in
+
+
+// differences between let and var
+function varTest() {
+  var x = 1
+  if (true) {
+    var x = 2  // same variable!
+    console.log(x)  // 2
+  }
+  console.log(x)  // 2
+}
+
+function letTest() {
+  let x = 1
+  if (true) {
+    let x = 2  // different variable
+    console.log(x)  // 2
+  }
+  console.log(x)  // 1
+}
+
+if (true) {
+	var x = "yes"
+}
+console.log(x) // "yes"
+
+if (true) {
+	let y = "yes"
+}
+console.log(y) // ReferenceError: y is not defined
